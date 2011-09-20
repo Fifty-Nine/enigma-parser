64f4d5a4030da1678afc4725bf7ad97e2d5d6e1a
Fifty-Nine <tim.prince@gmail.com>
2011-09-20 08:42:36 -0500

    Fix indentation

--- tools/AstViewer/AstModel.h
+++ tools/AstViewer/AstModel.h
@@ -10,27 +10,27 @@ namespace ast { class Node; }
 
 class AstModel : public QAbstractItemModel
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	AstModel(QObject *parent);
-	virtual ~AstModel();
+    AstModel(QObject *parent);
+    virtual ~AstModel();
 
-	void setFile(enigma::ast::Node *file);
+    void setFile(enigma::ast::Node *file);
 
-	virtual QModelIndex index(
-		int row, int column, const QModelIndex& parent=QModelIndex()) const;
-	virtual QModelIndex parent(const QModelIndex& idx) const;
-	virtual int rowCount(const QModelIndex& parent=QModelIndex()) const;
-	virtual int columnCount(const QModelIndex& parent=QModelIndex()) const;
-	virtual QVariant data(
-		const QModelIndex& idx, int role=Qt::DisplayRole) const;
-	virtual QVariant headerData(int section, 
-		Qt::Orientation orientation, int role=Qt::DisplayRole) const;
+    virtual QModelIndex index(
+        int row, int column, const QModelIndex& parent=QModelIndex()) const;
+    virtual QModelIndex parent(const QModelIndex& idx) const;
+    virtual int rowCount(const QModelIndex& parent=QModelIndex()) const;
+    virtual int columnCount(const QModelIndex& parent=QModelIndex()) const;
+    virtual QVariant data(
+        const QModelIndex& idx, int role=Qt::DisplayRole) const;
+    virtual QVariant headerData(int section, 
+        Qt::Orientation orientation, int role=Qt::DisplayRole) const;
 
 private:
     enigma::ast::Node *GetNode(const QModelIndex& idx) const;
-	enigma::ast::Node *m_root;
+    enigma::ast::Node *m_root;
 };
 
 #endif // ASTMODEL_H

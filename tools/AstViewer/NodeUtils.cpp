f9bee0024c4075dfde990c4238465451bef76eb1
Fifty-Nine <tim.prince@gmail.com>
2011-09-20 16:38:19 -0500

    Simplify node toString function object.

--- tools/AstViewer/NodeUtils.cpp
+++ tools/AstViewer/NodeUtils.cpp
@@ -13,7 +13,8 @@ class ToString : private enigma::visitors::NullVisitor
 {
 public:
     /*! Constructs a visitor for the given node. */
-    ToString(const enigma::ast::Node& node) 
+    ToString(const enigma::ast::Node& node) : 
+        data("{...}")
     {
         node.accept(*this);
     }
@@ -21,37 +22,22 @@ public:
     operator QString() { return data; }
 
 private:
-    void visit(const enigma::ast::Leaf& node);
-    void visit(const enigma::ast::Assignment& node);
-    void visit(const enigma::ast::AssignmentList& node);
-    void visit(const enigma::ast::ValueList& node);
+    void visit(const enigma::ast::Leaf& node)
+    {
+        data = node.token().toString();
+    }
+
+    void visit(const enigma::ast::Assignment& node)
+    {
+        QString format = "%1 = %2";
+        format = format.arg(node.left().token().toString());
+        format = format.arg(ToString(node.right()));
+
+        data = format;
+    }
 
     QString data;
 };
-
-void ToString::visit(const enigma::ast::Leaf& node)
-{
-    data = node.token().toString();
-}
-
-void ToString::visit(const enigma::ast::Assignment& node)
-{
-    QString format = "%1 = %2";
-    format = format.arg(node.left().token().toString());
-    format = format.arg(ToString(node.right()));
-
-    data = format;
-}
-
-void ToString::visit(const enigma::ast::AssignmentList&)
-{
-    data = "{...}";
-}
-
-void ToString::visit(const enigma::ast::ValueList&)
-{
-    data = "{...}";
-}
 } // namespace
 
 QString toString(const enigma::ast::Node& node)

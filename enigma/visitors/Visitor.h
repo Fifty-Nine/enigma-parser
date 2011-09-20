41e210308d4d659ee9985b0074629e080d675c29
Fifty-Nine <tim.prince@gmail.com>
2011-09-20 15:16:45 -0500

    Add a null visitor.

--- enigma/visitors/Visitor.h
+++ enigma/visitors/Visitor.h
@@ -18,11 +18,23 @@ namespace visitors
 class Visitor
 {
 public:
-	virtual ~Visitor() { }
+    virtual ~Visitor() { }
     virtual void visit(const ast::Leaf& node) = 0;
     virtual void visit(const ast::Assignment& node) = 0;
     virtual void visit(const ast::AssignmentList& node) = 0;
     virtual void visit(const ast::ValueList& node) = 0;
+};
+
+/*! A visitor that does nothing.
+ *  Useful for defining visitors that only handle 
+ *  a subset of the possible node types. */
+class NullVisitor : public Visitor
+{
+public:
+    virtual void visit(const ast::Leaf&) { }
+    virtual void visit(const ast::Assignment&) { }
+    virtual void visit(const ast::AssignmentList&) { }
+    virtual void visit(const ast::ValueList&) { }
 };
 
 } // namespace visitors
